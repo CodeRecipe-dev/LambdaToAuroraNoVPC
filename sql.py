2443c13b066ebb67e237cc8928652daa22ff49bf
Rohit Verma <rohitverma@live.ca>
2019-05-31 00:26:05 -0300

    use latest data api with sql parameters

--- sql.py
+++ sql.py
@@ -3,31 +3,39 @@ import os
 import boto3
 import pymysql
 
+
 def create_table(databaseName, awsSecretStoreArn, dbClusterOrInstanceArn):
     sqlStatement = 'create table records(record_id int NOT NULL, user_id int, data varchar(255), PRIMARY KEY (record_id))'
     _send_sql_to_rds(databaseName, awsSecretStoreArn, dbClusterOrInstanceArn, sqlStatement)
 
-
 def create_record(databaseName, awsSecretStoreArn, dbClusterOrInstanceArn, record_info):
-    sqlStatement = "INSERT INTO records(user_id, record_id, data) VALUES (1,{},'{}')".format(record_info['record_id'], pymysql.escape_string(record_info['data']))
-    _send_sql_to_rds(databaseName, awsSecretStoreArn, dbClusterOrInstanceArn, sqlStatement)
+    parameters = _get_record_id_data_parameters(record_info)
+    sqlStatement = "INSERT INTO records(user_id, record_id, data) VALUES (1, :record_id, :data);"
+    _send_sql_to_rds(databaseName, awsSecretStoreArn, dbClusterOrInstanceArn, sqlStatement, parameters)
 
 def update_record(databaseName, awsSecretStoreArn, dbClusterOrInstanceArn, record_info):
-    sqlStatement = 'UPDATE records SET data = '+json.dumps(record_info['data'])+' WHERE record_id = '+json.dumps(record_info['record_id'])+';'
+    parameters = _get_record_id_data_parameters(record_info)
+    sqlStatement = 'UPDATE records SET data = :data WHERE record_id = :record_id;'
     _send_sql_to_rds(databaseName, awsSecretStoreArn, dbClusterOrInstanceArn, sqlStatement)
 
 def get_records(databaseName, awsSecretStoreArn, dbClusterOrInstanceArn):
     sqlStatement = 'select * from records limit 5'
     response = _send_sql_to_rds(databaseName, awsSecretStoreArn, dbClusterOrInstanceArn, sqlStatement)
-    return response['sqlStatementResults'][0]['resultFrame']['records']
+    return response['records']
 
-def _send_sql_to_rds(databaseName, awsSecretStoreArn, dbClusterOrInstanceArn, sqlStatement):
+def _get_record_id_data_parameters(record_info):
+    record_id = {'name':'record_id', 'value':{'longValue': record_info['record_id']}}
+    data = {'name':'data', 'value':{'stringValue': record_info['data']}}
+    return [record_id, data]
+
+def _send_sql_to_rds(databaseName, awsSecretStoreArn, dbClusterOrInstanceArn, sqlStatement, parameters=[]):
     database_name = databaseName
     client = boto3.client('rds-data')
-    response = client.execute_sql(
-        awsSecretStoreArn=awsSecretStoreArn,
-        dbClusterOrInstanceArn=dbClusterOrInstanceArn,
+    response = client.execute_statement(
+        secretArn=awsSecretStoreArn,
+        resourceArn=dbClusterOrInstanceArn,
         database=databaseName,
-        sqlStatements=sqlStatement
+        parameters=parameters,
+        sql=sqlStatement
     )
     return response
